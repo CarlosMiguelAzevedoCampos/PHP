f034612e8c98dd2bcb944f75932b118ba143a284
Carlos Campos <carlosmiguelcampos1996@gmail.com>
2018-02-09 00:49:03 +0000

    Update class.Edit.php

--- class.Edit.php
+++ class.Edit.php
@@ -9,10 +9,10 @@ class Edit
 	private $db;
 	use Database;
     
-    //Construtor da classe.
+   	 //Construtor da classe.
 	function __construct()
 	{
-        //Se não conseguir conectar ele mata a ligação
+        	//Se não conseguir conectar ele mata a ligação
 		if (!$this->db_connect()) {
 			die("Falha na ligação");
 		}
@@ -23,13 +23,13 @@ class Edit
 	protected function Actions()
 	{
         //Por exemplo, aqui, o link terá o seguinte aspeto.. http://localhost/class.Edit.php?action=edit&id=1&nome=Carlos&idade=18
-		if ($_GET["action"] == "edit") {
-            $id = $this->db->real_escape_string($_GET['id']); 
-            $nome = $this->db->real_escape_string($_GET['nome']); 
-            $idade = $this->db->real_escape_string($_GET['idade']);
-            /* o $this->db->real_escape_string, serve para segurança, pois retira os caracters especiais. Por exemplo #999
-            aceite*/
-            $this->Edit($id,$nome, $idade);
+	if ($_GET["action"] == "edit") {
+		    $id = $this->db->real_escape_string($_GET['id']); 
+		    $nome = $this->db->real_escape_string($_GET['nome']); 
+		    $idade = $this->db->real_escape_string($_GET['idade']);
+		    /* o $this->db->real_escape_string, serve para segurança, pois retira os caracters especiais. Por exemplo #999
+		    aceite*/
+		    $this->Edit($id,$nome, $idade);
         }
     }
 
@@ -38,16 +38,16 @@ class Edit
 		if ($query = $this->db->prepare('UPDATE bd_table set nome=?, idade=? where id=?')) {
 			$query->bind_param("sii", $nome, $idade, $id);
 			if ($query->execute()) { // se executou irá colocar no resultado_do_editar, done
-               $response["resultado_do_editar"] = "done";
-    		}
+               			$response["resultado_do_editar"] = "done";
+    			}
 			else{// se não executou irá colocar no resultado_do_editar, execute-error
-		    	$response["resultado_do_editar"] = "execute-error";
+		    		$response["resultado_do_editar"] = "execute-error";
 			}
         }
         else {// se não entrar no if por alguma razão, o resultado_do_editar, terá false.
-			$response["resultado_do_inserir"] = "false";
-		}
-		$response["type"] = "editar_dados"; //aqui defino o tipo de resposta, se é de editar, ou apagar alguma coisa da bd
-		echo json_encode($response);
+		$response["resultado_do_editar"] = "false";
+	}
+	$response["type"] = "editar_dados"; //aqui defino o tipo de resposta, se é de editar, ou apagar alguma coisa da bd
+	echo json_encode($response);
     }
 }
